d5bfe47dc45fd02f3f0d208e0b0f2d1e548c8466
daboo <daboo@a91246af-f21b-0410-bd1c-c3c7fc455132>
2007-07-08 17:07:58 +0000

    tabs->spaces. Add option to undo license change.

--- Mulberry/trunk/license-change.py
+++ Mulberry/trunk/license-change.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+import getopt
 import os
 import sys
 
@@ -21,58 +22,92 @@ newlicense = """/*
 
 """
 
-def process_directory(path):
-	for file in os.listdir(path):
-		fpath = os.path.join(path, file)
-		if os.path.isfile(fpath):
-			process_file(fpath)
-		elif os.path.isdir(fpath):
-			process_directory(fpath)
+def process_directory(path, delete_unlicensed, undo):
+    for file in os.listdir(path):
+        fpath = os.path.join(path, file)
+        if os.path.isfile(fpath):
+            if undo:
+                undo_file(fpath)
+            else:
+                process_file(fpath, delete_unlicensed)
+        elif os.path.isdir(fpath):
+            process_directory(fpath, delete_unlicensed, undo)
 
-def process_file(fpath):
-	if (fpath.endswith(".cp") or
-		fpath.endswith(".cc") or
-		fpath.endswith(".cpp") or
-		fpath.endswith(".c") or
-		fpath.endswith(".h")):
-		replace_license(fpath)
+def undo_file(fpath):
+    if (fpath.endswith(".cp.unlicensed") or
+        fpath.endswith(".cc.unlicensed") or
+        fpath.endswith(".cpp.unlicensed") or
+        fpath.endswith(".c.unlicensed") or
+        fpath.endswith(".h.unlicensed")):
+        fpath = fpath[:-11]
+        print fpath
+        os.rename(fpath, "%s.licensed" % fpath)
+        os.rename("%s.unlicensed" % fpath, fpath)
+        os.remove("%s.licensed" % fpath)
+
+def process_file(fpath, delete_unlicensed):
+    if delete_unlicensed and \
+       (fpath.endswith(".cp.unlicensed") or
+        fpath.endswith(".cc.unlicensed") or
+        fpath.endswith(".cpp.unlicensed") or
+        fpath.endswith(".c.unlicensed") or
+        fpath.endswith(".h.unlicensed")):
+        os.remove(fpath)
+    elif not delete_unlicensed and \
+         (fpath.endswith(".cp") or
+          fpath.endswith(".cc") or
+          fpath.endswith(".cpp") or
+          fpath.endswith(".c") or
+          fpath.endswith(".h")):
+        replace_license(fpath)
 
 def replace_license(fpath):
-	print fpath
-	fin = open(fpath, "r")
-	fout = open("%s.licensed" % fpath, "w")
+    print fpath
+    fin = open(fpath, "rU")
+    fout = open("%s.licensed" % fpath, "w")
 
-	first = True
-	ignore = False
-	reallyignore = False
-	ignored = []
-	for line in fin:
-		if first:
-			fout.write(newlicense)
-			if line.startswith("/*"):
-				ignore = True
-		first = False
-		if not ignore:
-			fout.write(line)
-		else:
-			ignored.append(line)
-		if ignore:
-			if line.find("Copyright") != -1:
-				reallyignore = True
-			if line.find("*/") != -1:
-				ignore = False
-				if not reallyignore:
-					fout.write("".join(ignored))
-	
-	fin.close()
-	fout.close()
-	os.rename(fpath, "%s.unlicensed" % fpath)
-	os.rename("%s.licensed" % fpath, fpath)
+    first = True
+    ignore = False
+    reallyignore = False
+    ignored = []
+    for line in fin:
+        if first:
+            fout.write(newlicense)
+            if line.startswith("/*"):
+                ignore = True
+        first = False
+        if not ignore:
+            fout.write(line)
+        else:
+            ignored.append(line)
+        if ignore:
+            if line.find("Copyright") != -1:
+                reallyignore = True
+            if line.find("*/") != -1:
+                ignore = False
+                if not reallyignore:
+                    fout.write("".join(ignored))
+    
+    fin.close()
+    fout.close()
+    os.rename(fpath, "%s.unlicensed" % fpath)
+    os.rename("%s.licensed" % fpath, fpath)
 
 if __name__ == "__main__":
-	paths = ("Sources_Common", "Linux_v2",)
-	#paths = ("Plug-ins", )
 
-	for path in paths:
-		process_directory(path)
-			
+    delete_unlicensed = False
+    undo = False
+
+    options, args = getopt.getopt(sys.argv[1:], "ru")
+
+    for option, value in options:
+        if option == "-r":
+            delete_unlicensed = True
+        elif option == "-r":
+            undo = True
+
+    paths = args
+
+    for path in paths:
+        process_directory(path, delete_unlicensed, undo)
+            
